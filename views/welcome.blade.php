6e5a323b2c03e66a94c97d8b42c919f40ec9add7
ChokitoLoquendo <44873797+ChokitoLoquendo@users.noreply.github.com>
2023-11-30 01:26:16 -0400

    Add files via upload

---
+++ views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+        <title>Laravel</title>
+<form action=""></form>
+        <style>
+            body{
+                background-color:aqua
+            }
+        </style>
+    </body>
+</html>
